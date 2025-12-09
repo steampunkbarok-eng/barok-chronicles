d011c124cca2f5f91513db181814c392cdc00336
steampunkbarok-eng <steampunk.barok@gmail.com>
2025-12-09 21:22:57 +0100

    Update titres.ts

--- src/data/titres.ts
+++ src/data/titres.ts
@@ -6,7 +6,7 @@ export interface Titre {
 }
 
 export const titresCarrieres: Titre[] = [
-  { nom: "Adepte", cout: 2, incompatible: "Planaire,Ordre Chevaliers Suie (LAM), Sectaire ésotérico-magique, Démuni", prerequis: "Culte en jeu" },
+  { nom: "Adepte", cout: 2, incompatible: "Planaire, Ordre Chevaliers Suie (LAM), Sectaire ésotérico-magique, Démuni", prerequis: "Culte en jeu" },
   { nom: "Agent d'une ONG", cout: 2, incompatible: "Planaire, Porteur de Rune, Pirate, Chef de Bande Organisée, Démuni", prerequis: "" },
   { nom: "Ancien Esclave", cout: 2, incompatible: "Démuni", prerequis: "Background + Entrave + Évasion" },
   { nom: "Archiviste des Secrets", cout: 2, incompatible: "Chef de Bande Organisée, Pirate, Épervier, Contrebandier, Démuni", prerequis: "Alphabétisation commune" },
@@ -20,13 +20,13 @@ export const titresCarrieres: Titre[] = [
   { nom: "Épervier", cout: 2, incompatible: "Tous fonctionnaires, Agent d'une ONG, Corsaire elfique", prerequis: "Background + Capitaine Navire/Navire-Volant" },
   { nom: "Garde du Corps", cout: 2, incompatible: "Autres fonctionnaires, Chef de Bande Organisée, Enquêteur, Pirate, Épervier, Prévôt du Crépuscule, Mercenaire  Peau-olive, Sergent de l'ombre, Sectaire ésotérico-magique", prerequis: "" },
   { nom: "Gestionnaire de Dépôt", cout: 2, incompatible: "", prerequis: "Menuisier" },
-  { nom: "Greffier", cout: 2, incompatible: "Capitaine Navire/Navire-Volant, Chef de Bande Organisée, LAM, Mercenaire Peau-olive, Pirates, Planaires, Prévôt du Crépuscule, Sergent de l'ombre, Milicien, Enquêteur", prerequis: "Alphab. avancée + Solde" },
+  { nom: "Greffier", cout: 2, incompatible: "Capitaine Navire, Capitaine Navire-Volant, Chef de Bande Organisée, LAM, Mercenaire Peau-olive, Pirates, Planaires, Prévôt du Crépuscule, Sergent de l'ombre, Milicien, Enquêteur", prerequis: "Alphab. avancée + Solde" },
   { nom: "Mercenaire Peau-olive", cout: 2, incompatible: "Chef de Bande Organisée, LAM, Corsaire elfique, Épervier, Garde du Corps, Greffier, Pirate, Planaire, Porteur de Rune, Prévôt du Crépuscule, Sergent de l'ombre, Milicien, Magistrat, Démuni", prerequis: "Orcs/Gobelins/Trolls + Background" },
   { nom: "Noblesse de Sang", cout: 2, incompatible: "Démuni", prerequis: "Background liens noblesse" },
   { nom: "Ordre Chevaliers Suie (LAM)", cout: 2, incompatible: "Adepte, Planaire, Porteur de Rune, Sectaire, Magie : cérémonialiste, clerc, initié, tisseur, ritualiste", prerequis: "Haine magie + Compétence technologie/mécanique/forgeron" },
   { nom: "Pirate", cout: 2, incompatible: "Agent d'une ONG, LAM, Corsaire elfique, Épervier, Greffier, Mercenaire Peau-olive, Porteur de Rune, Milicien, Magistrat, Démuni", prerequis: "Background + Code Piraterie" },
   { nom: "Planaire", cout: 2, incompatible: "Adepte, Ordre Chevaliers Suie (LAM), Porteur de Rune, titres ancrés Barok", prerequis: "Background extraplanaire" },
-  { nom: "Porteur de Rune", cout: 2, incompatible: "Capitaine Navire/Navire-Volant, Ordre Chevaliers Suie (LAM), Planaire, Pirate, Mercenaire Peau-olive, Agent d'une ONG", prerequis: "Nains/Gnomes/Sautaises + Forgeron" },
+  { nom: "Porteur de Rune", cout: 2, incompatible: "Capitaine Navire, Capitaine Navire-Volant, Ordre Chevaliers Suie (LAM), Planaire, Pirate, Mercenaire Peau-olive, Agent d'une ONG", prerequis: "Nains/Gnomes/Sautaises + Forgeron" },
   { nom: "Prévôt du Crépuscule", cout: 2, incompatible: "Corsaire elfique, Enquêteur, Garde du Corps, Greffier, Magistrat, Mercenaire Peau-olive", prerequis: "Filouterie" },
   { nom: "Sectaire ésotérico-magique", cout: 2, incompatible: "Adepte, Garde du Corps, Ordre Chevaliers Suie (LAM)", prerequis: "Mystique"},
   { nom: "Sergent de l'ombre", cout: 2, incompatible: "Corsaire elfique, Garde du Corps, Greffier, Mercenaire Peau-olive, Milicien, Magistrat, Enquêteur Public", prerequis: "Filouterie + deux compétences Martiales"}
